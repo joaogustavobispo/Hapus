11f8ca771c82d19984579f9f0b7d85cd3b54845b
João Gustavo <joaogustavobispo15@gmail.com>
2020-10-13 13:09:03 -0300

    Landing Page Home - Feita

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+// import
+import React from "react";
+
+import Routes from './router';
+
+// JSX - Forma de colocar html dentro de JavaScript
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
---
+++ web/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; //Componente - Retorna uma função em html
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
